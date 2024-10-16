a7b14aa27fbf0b3905223734dfe5187feb86d047
Viola G <77071461+viola166@users.noreply.github.com>
2024-10-16 15:26:17 -0300

    changed class name
    for better functionality inference

--- library/SensorAPI/DataInfo.cs
+++ library/SensorAPI/DataInfo.cs
@@ -1,12 +1,12 @@
 namespace SensorAPI{
-    public class DataInfo {
+    public class SignalsWithData {
         // which sensor captured the data
         public string sensorName;
         // which kind of data is provided
         public string dataChannel;
         // value of the data
         public double data;
-        public DataInfo(string sensorName, string dataChannel, double data) {
+        public SignalsWithData(string sensorName, string dataChannel, double data) {
             this.sensorName = sensorName;
             this.dataChannel = dataChannel;
             this.data = data;
