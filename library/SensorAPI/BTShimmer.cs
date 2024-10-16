b1adfe6f43bf411f64e2304bad57a69302677ecd
Viola G <77071461+viola166@users.noreply.github.com>
2024-10-16 15:24:00 -0300

    updated Streaming() method

--- library/SensorAPI/BTShimmer.cs
+++ library/SensorAPI/BTShimmer.cs
@@ -1,4 +1,4 @@
-﻿// using InTheHand.Net.Bluetooth for Bluetooth RFCOMM
+// using InTheHand.Net.Bluetooth for Bluetooth RFCOMM
 
 using InTheHand.Net.Bluetooth;
 using InTheHand.Net;
@@ -10,7 +10,7 @@ using System.ComponentModel.DataAnnotations;
 
 
 namespace SensorAPI{
-    public class BTShimmer : ShimmerLogAndStream, SensorInfos
+    public class BTShimmer : ShimmerLogAndStream, SensorUnit
     {
 
         protected String ShimmerBluetoothAddress;
@@ -23,7 +23,24 @@ namespace SensorAPI{
         String devID;
         protected Thread StreamThread;
         string[] dataChannelsToStream;
+        public readonly object lockObj = new object();
 
+
+        private List<SignalsWithData> _lastStreamedData = new List<SignalsWithData>();
+        public List<SignalsWithData> LastStreamedData 
+        { 
+            get 
+            { 
+                lock(lockObj) {
+                    return _lastStreamedData;
+                }
+            }
+            set {
+                lock(lockObj) {
+                    _lastStreamedData = value;
+                }
+            }
+        }
 
         // called in constructors
         public void Initializing(String devID, String bluetoothAddress) {
@@ -137,42 +154,166 @@ namespace SensorAPI{
         }
 
         public void Streaming() {
-            List<DataInfo> dataInfos = new List<DataInfo>();
+            List<SignalsWithData> fillStreamedData = new List<SignalsWithData>();
             // to store all sensor information in ShimmerBluetooth attributes call ReadData()
             // ReadData() will read bytes and interpret them accordingly if state == connected
             // ReadData() will read bytes and build/store object cluster (KeepObjectCluster) if state == streaming
             while (!StopReading) {
+                fillStreamedData.Clear();
                 for (int i = 0; i < dataChannelsToStream.Length; i++) {
                     // List<string> allStoredSignalNames = KeepObjectCluster.GetNames();
                     List<double> signalDataTypes = KeepObjectCluster.GetData();
-                    if (dataChannelsToStream[i] == Shimmer3Configuration.SignalNames.ECG_LA_RA) {
-                        Console.WriteLine("in ECG_LA_RA");
+                    
+                    if (dataChannelsToStream[i] == Shimmer3Configuration.SignalNames.GYROSCOPE_X) {
                         if (KeepObjectCluster != null) {
-                            
+
                             // get Index of specified signal in the list of signal names from the last object cluster
-                            int indexECG_LA_RA = getSignalIndex(Shimmer3Configuration.SignalNames.ECG_LA_RA);
-                            DataInfo ecg_la_ra_info = new DataInfo(devID, dataChannelsToStream[i], signalDataTypes[indexECG_LA_RA]);
-                            dataInfos.Add(ecg_la_ra_info);
+                            int indexGyroscope_X = getSignalIndex(Shimmer3Configuration.SignalNames.GYROSCOPE_X);
+                            SignalsWithData gyro_x_info = new SignalsWithData(devID, dataChannelsToStream[i], signalDataTypes[indexGyroscope_X]);
+                            fillStreamedData.Add(gyro_x_info);
                         }
-                        break;
+                    } else if (dataChannelsToStream[i] == Shimmer3Configuration.SignalNames.GYROSCOPE_Y) {
+                        if (KeepObjectCluster != null) {
+
+                            int indexGyroscope_Y = getSignalIndex(Shimmer3Configuration.SignalNames.GYROSCOPE_Y);
+                            SignalsWithData gyro_y_info = new SignalsWithData(devID, dataChannelsToStream[i], signalDataTypes[indexGyroscope_Y]);
+                            fillStreamedData.Add(gyro_y_info);
+                        }
                     } else if (dataChannelsToStream[i] == Shimmer3Configuration.SignalNames.GYROSCOPE_Z) {
-                        Console.WriteLine("in GYROSCOPE_Z");
                         if (KeepObjectCluster != null) {
 
                             int indexGyroscope_Z = getSignalIndex(Shimmer3Configuration.SignalNames.GYROSCOPE_Z);
-                            DataInfo gyro_z_info = new DataInfo(devID, dataChannelsToStream[i], signalDataTypes[indexGyroscope_Z]);
-                            dataInfos.Add(gyro_z_info);
+                            SignalsWithData gyro_z_info = new SignalsWithData(devID, dataChannelsToStream[i], signalDataTypes[indexGyroscope_Z]);
+                            fillStreamedData.Add(gyro_z_info);
                         }
-                        break;
-                    } // else if...
+                    } else if (dataChannelsToStream[i] == Shimmer3Configuration.SignalNames.LOW_NOISE_ACCELEROMETER_X) {
+                        if (KeepObjectCluster != null) {
+
+                            int indexLNAccel_X = getSignalIndex(Shimmer3Configuration.SignalNames.LOW_NOISE_ACCELEROMETER_X);
+                            SignalsWithData ln_accel_x_info = new SignalsWithData(devID, dataChannelsToStream[i], signalDataTypes[indexLNAccel_X]);
+                            fillStreamedData.Add(ln_accel_x_info);
+                        }
+                    } else if (dataChannelsToStream[i] == Shimmer3Configuration.SignalNames.LOW_NOISE_ACCELEROMETER_Y) {
+                        if (KeepObjectCluster != null) {
+
+                            int indexLNAccel_Y = getSignalIndex(Shimmer3Configuration.SignalNames.LOW_NOISE_ACCELEROMETER_Y);
+                            SignalsWithData ln_accel_y_info = new SignalsWithData(devID, dataChannelsToStream[i], signalDataTypes[indexLNAccel_Y]);
+                            fillStreamedData.Add(ln_accel_y_info);
+                        }
+                    } else if (dataChannelsToStream[i] == Shimmer3Configuration.SignalNames.LOW_NOISE_ACCELEROMETER_Z) {
+                        if (KeepObjectCluster != null) {
+
+                            int indexLNAccel_Z = getSignalIndex(Shimmer3Configuration.SignalNames.LOW_NOISE_ACCELEROMETER_Z);
+                            SignalsWithData ln_accel_z_info = new SignalsWithData(devID, dataChannelsToStream[i], signalDataTypes[indexLNAccel_Z]);
+                            fillStreamedData.Add(ln_accel_z_info);
+                        }
+                    } else if (dataChannelsToStream[i] == Shimmer3Configuration.SignalNames.WIDE_RANGE_ACCELEROMETER_X) {
+                        if (KeepObjectCluster != null) {
+
+                            int indexWDAccel_X = getSignalIndex(Shimmer3Configuration.SignalNames.WIDE_RANGE_ACCELEROMETER_X);
+                            SignalsWithData wr_accel_x_info = new SignalsWithData(devID, dataChannelsToStream[i], signalDataTypes[indexWDAccel_X]);
+                            fillStreamedData.Add(wr_accel_x_info);
+                        }
+                    } else if (dataChannelsToStream[i] == Shimmer3Configuration.SignalNames.WIDE_RANGE_ACCELEROMETER_Y) {
+                        if (KeepObjectCluster != null) {
+
+                            int indexWDAccel_Y = getSignalIndex(Shimmer3Configuration.SignalNames.WIDE_RANGE_ACCELEROMETER_Y);
+                            SignalsWithData wr_accel_y_info = new SignalsWithData(devID, dataChannelsToStream[i], signalDataTypes[indexWDAccel_Y]);
+                            fillStreamedData.Add(wr_accel_y_info);
+                        }
+                    } else if (dataChannelsToStream[i] == Shimmer3Configuration.SignalNames.WIDE_RANGE_ACCELEROMETER_Z) {
+                        if (KeepObjectCluster != null) {
+
+                            int indexWDAccel_Z = getSignalIndex(Shimmer3Configuration.SignalNames.WIDE_RANGE_ACCELEROMETER_Z);
+                            SignalsWithData wr_accel_z_info = new SignalsWithData(devID, dataChannelsToStream[i], signalDataTypes[indexWDAccel_Z]);
+                            fillStreamedData.Add(wr_accel_z_info);
+                        }
+                    } else if (dataChannelsToStream[i] == Shimmer3Configuration.SignalNames.MAGNETOMETER_X) {
+                        if (KeepObjectCluster != null) {
+
+                            int indexMag_X = getSignalIndex(Shimmer3Configuration.SignalNames.MAGNETOMETER_X);
+                            SignalsWithData mag_x_info = new SignalsWithData(devID, dataChannelsToStream[i], signalDataTypes[indexMag_X]);
+                            fillStreamedData.Add(mag_x_info);
+                        }
+                    } else if (dataChannelsToStream[i] == Shimmer3Configuration.SignalNames.MAGNETOMETER_Y) {
+                        if (KeepObjectCluster != null) {
+
+                            int indexMag_Y = getSignalIndex(Shimmer3Configuration.SignalNames.MAGNETOMETER_Y);
+                            SignalsWithData mag_y_info = new SignalsWithData(devID, dataChannelsToStream[i], signalDataTypes[indexMag_Y]);
+                            fillStreamedData.Add(mag_y_info);
+                        }
+                    } else if (dataChannelsToStream[i] == Shimmer3Configuration.SignalNames.MAGNETOMETER_Z) {
+                        if (KeepObjectCluster != null) {
+
+                            int indexMag_Z = getSignalIndex(Shimmer3Configuration.SignalNames.MAGNETOMETER_Z);
+                            SignalsWithData mag_z_info = new SignalsWithData(devID, dataChannelsToStream[i], signalDataTypes[indexMag_Z]);
+                            fillStreamedData.Add(mag_z_info);
+                        }
+                    } else if (dataChannelsToStream[i] == Shimmer3Configuration.SignalNames.PRESSURE) {
+                        if (KeepObjectCluster != null) {
+
+                            int indexPressure = getSignalIndex(Shimmer3Configuration.SignalNames.PRESSURE);
+                            SignalsWithData pressure_info = new SignalsWithData(devID, dataChannelsToStream[i], signalDataTypes[indexPressure]);
+                            fillStreamedData.Add(pressure_info);
+                        }
+                    } else if (dataChannelsToStream[i] == Shimmer3Configuration.SignalNames.TEMPERATURE) {
+                        if (KeepObjectCluster != null) {
+
+                            int indexTemp = getSignalIndex(Shimmer3Configuration.SignalNames.TEMPERATURE);
+                            SignalsWithData temp_info = new SignalsWithData(devID, dataChannelsToStream[i], signalDataTypes[indexTemp]);
+                            fillStreamedData.Add(temp_info);
+                        }
+                    } else if (dataChannelsToStream[i] == Shimmer3Configuration.SignalNames.GSR) {
+                        if (KeepObjectCluster != null) {
+
+                            int indexGSR = getSignalIndex(Shimmer3Configuration.SignalNames.GSR);
+                            SignalsWithData gsr_info = new SignalsWithData(devID, dataChannelsToStream[i], signalDataTypes[indexGSR]);
+                            fillStreamedData.Add(gsr_info);
+                        }
+                    } else if (dataChannelsToStream[i] == Shimmer3Configuration.SignalNames.GSR_CONDUCTANCE) {
+                        if (KeepObjectCluster != null) {
+
+                            int indexGSR_C = getSignalIndex(Shimmer3Configuration.SignalNames.GSR_CONDUCTANCE);
+                            SignalsWithData gsr_con_info = new SignalsWithData(devID, dataChannelsToStream[i], signalDataTypes[indexGSR_C]);
+                            fillStreamedData.Add(gsr_con_info);
+                        }
+                    } else if (dataChannelsToStream[i] == Shimmer3Configuration.SignalNames.EXG1_CH1) {
+                        if (KeepObjectCluster != null) {
+
+                            int indexEXG1_CH1 = getSignalIndex(Shimmer3Configuration.SignalNames.EXG1_CH1);
+                            SignalsWithData exg1_ch1_info = new SignalsWithData(devID, dataChannelsToStream[i], signalDataTypes[indexEXG1_CH1]);
+                            fillStreamedData.Add(exg1_ch1_info);
+                        }
+                    } else if (dataChannelsToStream[i] == Shimmer3Configuration.SignalNames.EXG1_CH2) {
+                        if (KeepObjectCluster != null) {
+
+                            int indexEXG1_CH2 = getSignalIndex(Shimmer3Configuration.SignalNames.EXG1_CH2);
+                            SignalsWithData exg1_ch2_info = new SignalsWithData(devID, dataChannelsToStream[i], signalDataTypes[indexEXG1_CH2]);
+                            fillStreamedData.Add(exg1_ch2_info);
+                        }
+                    } else if (dataChannelsToStream[i] == Shimmer3Configuration.SignalNames.EXG2_CH1) {
+                        if (KeepObjectCluster != null) {
+
+                            int indexEXG2_CH1 = getSignalIndex(Shimmer3Configuration.SignalNames.EXG2_CH1);
+                            SignalsWithData exg2_ch1_info = new SignalsWithData(devID, dataChannelsToStream[i], signalDataTypes[indexEXG2_CH1]);
+                            fillStreamedData.Add(exg2_ch1_info);
+                        }
+                    } else if (dataChannelsToStream[i] == Shimmer3Configuration.SignalNames.EXG2_CH2) {
+                        if (KeepObjectCluster != null) {
+
+                            int indexEXG2_CH2 = getSignalIndex(Shimmer3Configuration.SignalNames.EXG2_CH2);
+                            SignalsWithData exg2_ch2_info = new SignalsWithData(devID, dataChannelsToStream[i], signalDataTypes[indexEXG2_CH2]);
+                            fillStreamedData.Add(exg2_ch2_info);
+                        }
+                    }
 
                 }
-                System.Threading.Thread.Sleep(1000);
-
-                DataInfo lastDataInfo = dataInfos.Last();
-                Console.WriteLine(lastDataInfo.dataChannel + ": " + lastDataInfo.data);
+                
+                lock(lockObj) {
+                    LastStreamedData.Clear();
+                    LastStreamedData.AddRange(fillStreamedData);
+                }
             }
-            DataInfo[] dataInfosArray = dataInfos.ToArray();
         }
 
 
