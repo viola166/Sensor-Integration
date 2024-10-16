24e7c9764e4053cb43107f661cc3e801f87988d0
Viola G <77071461+viola166@users.noreply.github.com>
2024-10-16 15:28:35 -0300

    updated class name for better functionality inference

--- library/SensorAPI/SensorUnit.cs
+++ library/SensorAPI/SensorUnit.cs
@@ -1,12 +1,14 @@
 namespace SensorAPI{
-    public interface SensorInfos {
+    public interface SensorUnit {
+
+        List<SignalsWithData> LastStreamedData { get; set; }
         
         // stream the specified data by a specified sensor (the object which calls GetData)
         // probably change return type to void
         void StreamSignalData(string[] dataChannels);
 
         // collect the specified data from the logging and extend already collectedData
-        // DataInfo[] CollectSignalData(string[] dataChannels, DataInfo[] collectedData);
+        // SignalsWithData[] CollectSignalData(string[] dataChannels, DataInfo[] collectedData);
 
 
     }
