f82b8c325f7eac741fab994c76abbd620b9e36ba
Viola G <77071461+viola166@users.noreply.github.com>
2024-12-02 14:13:05 -0300

    Add files via upload

---
+++ library/SensorAPI/MuseLibWrapper/MuseLibWrapper/MuseListener.cpp
@@ -0,0 +1,1 @@
+#include "MuseListener.h"
---
+++ library/SensorAPI/MuseLibWrapper/MuseLibWrapper/MuseListener.h
@@ -0,0 +1,21 @@
+//#pragma once
+//
+//#include "include-muse\muse.h"
+//
+//using namespace System;
+//
+//namespace MuseLibWrapper {
+//
+//    public ref class MuseListener {
+//
+//    public:
+//        // Managed method to return a pointer to the MuseManager instance
+//        System::IntPtr GetMuseManager();
+//    };
+//
+//    // Declaration of the native function, exported from the C++ code
+//    extern "C" {
+//        __declspec(dllexport) interaxon::bridge::MuseManagerWindows* Get_Muse_Manager();
+//    }
+//}
+
